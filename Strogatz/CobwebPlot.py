2fcb557f4d2ae601f868d659f91203419c3a0729
Llewelyn Richards-Ward <llewelyn62@icloud.com>
2017-12-06 08:12:40 +1300

    Add Ch11 and Ch12

--- Strogatz/CobwebPlot.py
+++ Strogatz/CobwebPlot.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 # here is the function we want to iterate
 # mu is a possible parameter
-def func(x,mu):
-	return mu*np.sin(x*(1.0*np.pi)) #mu*x*(1-x)
+def func(x,mu,c4):
+	return x*(2*np.pi/mu) + c4#mu*np.sin(x*(1.0*np.pi)) #mu*x*(1-x)
 
 # return f^n(x)
-def func_n(x,mu,n):
-	for i in range(0,n):
-		x = func(x,mu)
+def func_n(x,mu,c4,n):
+	for i in range(-n,n):
+		x = func(x,mu,c4)
 
 	return x
 # here is "plotting graphical" or "cobweb" for an interated map
@@ -19,33 +19,40 @@ def func_n(x,mu,n):
 #  ... (f^i(x), f^(i+1)(x)),(f^(i+1),f^(i+1)) to i=n
 # initial x0, mu is a parameter to pass to function
 # connect up points n times, this is 2n pairs of points
-def plot_graphical(x0,mu,n):
+def plot_graphical(x0,mu,c4, n):
 	xv = np.linspace(0.0,1.0,2*n)  # create array for points xvalue 
 	yv = np.linspace(0.0,1.0,2*n)  # create array for points yvalue 
 	x =x0
 	for i in range(0,n):  #iterate
 		xv[2*i] = x  # first point is (x,f(x))
-		x = func(x,mu)
+		x = func(x,mu,c4)
 		yv[2*i] = x
 		xv[2*i+1] = x #second point is (f(x),f(x))
 		yv[2*i+1] = x
 	plt.plot(xv,yv,'b')  # connect up all these points blue
 
 plt.figure()
-plt.xlabel('x')
-plt.ylabel('f(x)')
-fac=1.01
-xmax = 1.00
-xmin =-0.01
-ymax = 1.01
-ymin =-0.01
-plt.axis([xmin*fac,xmax*fac,ymin*fac,ymax*fac])
-xcon = np.arange(xmin, xmax, 0.01)   # to plot function 
-plt.plot(xcon,xcon, 'g')             #y=x plotted gree
+plt.xlabel('r')
+plt.ylabel('P(r)')
+plt.xticks([-np.pi, -.5*np.pi,0., .5*np.pi, np.pi,],[r"$-\pi$",r"$\frac{-1}{2}\pi$","$0$", r"$\frac{1}{2}\pi$",
+                     r"$\pi$"])
 
-mu=.3
-ycon = func(xcon,mu)                 # function computed
-plt.plot(xcon,ycon, 'r')             # function plotted red
-plot_graphical(0.3,mu,50)            # cobweb plot, 0.3 is initial condition
+fac=1.01
+xmax = np.pi
+xmin =-np.pi
+#ymax = 1.01
+#ymin =0.01
+#plt.axis([xmin*fac,xmax*fac,ymin*fac,ymax*fac])
+xcon = np.arange(xmin, xmax, 0.01)   # to plot function 
+plt.plot(xcon,xcon, 'g',label=r'P(r) = r')             #y=x plotted green
+
+mu=10
+c4= .5
+ycon = func(xcon,mu,c4)                 # function computed
+plt.plot(xcon,ycon, 'r', label="P(r)")             # function plotted red
+plot_graphical(xmin,mu,c4,10)            # cobweb plot, 0.3 is initial condition
+plot_graphical(xmax,mu,c4,10)            # cobweb plot, 0.3 is initial condition
+plt.title('Strogatz Figure 8.7.3')
+plt.grid()
+plt.legend()
 plt.show()
-#X, Y =plot_graphical(0.3,mu,5)
